54e1c7d9a0b15bef317e7c44df45bbcaabde40fd
samol.bav <samol.bav@prasac.com.kh>
2023-12-29 15:54:20 +0700

    new connect to github

---
+++ app/src/main/java/com/example/kbprasacbank/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.kbprasacbank
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.window.SplashScreen
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kbprasacbank.ui.theme.KBPrasacBankTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KBPrasacBankTheme {
+                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+                SplashScreen()
+//                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSlashScreen() {
+    KBPrasacBankTheme {
+        SplashScreen()
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(modifier = modifier) {
+        Column(
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+            )
+        }
+    }
+}
